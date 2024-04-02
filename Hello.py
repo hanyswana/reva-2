a76698aba1a3f0c267ce55b7e8e414fe3b68d2f9
hanyswana <146700525+hanyswana@users.noreply.github.com>
2024-04-02 10:41:45 +0800

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -34,16 +34,34 @@ def csv_data(uploaded_file):
         
 
 def load_model(model_dir):
-    model = tf.saved_model.load(model_dir)
-    return model
+    if model_dir.endswith('.tflite'):  # Check if model is a TensorFlow Lite model
+        # Load TensorFlow Lite model
+        interpreter = tf.lite.Interpreter(model_path=model_dir)
+        interpreter.allocate_tensors()
+        return interpreter
+    else:
+        # Load TensorFlow SavedModel
+        model = tf.saved_model.load(model_dir)
+        return model
+
 
 def predict_with_model(model, input_data):
-
-    input_array = input_data.to_numpy(dtype='float64')
-    input_array_reshaped = input_array.reshape(-1, 19)  # Adjust to match the number of features your model expects
-    input_tensor = tf.convert_to_tensor(input_array_reshaped, dtype=tf.float64)
-    predictions = model(input_tensor)
-    return predictions.numpy()  # Convert predictions to numpy array if needed
+    if isinstance(model, tf.lite.Interpreter):  # Check if model is TensorFlow Lite Interpreter
+        input_details = model.get_input_details()
+        output_details = model.get_output_details()
+        
+        # Assuming input_data is already in the correct shape and type
+        model.set_tensor(input_details[0]['index'], input_data)
+        model.invoke()
+        predictions = model.get_tensor(output_details[0]['index'])
+        return predictions  # This will be a numpy array
+    else:
+        # Existing prediction code for TensorFlow SavedModel
+        input_array = input_data.to_numpy(dtype='float64')
+        input_array_reshaped = input_array.reshape(-1, 19)  # Adjust to match the number of features your model expects
+        input_tensor = tf.convert_to_tensor(input_array_reshaped, dtype=tf.float64)
+        predictions = model(input_tensor)
+        return predictions.numpy()  # Convert predictions to numpy array if needed
 
 def main():
 
@@ -55,32 +73,20 @@ def main():
     
     # Define model paths with labels
     model_paths_with_labels = [
-        ('R39', 'reva-lablink-hb-125-(original-data).csv_r2_0.39_2024-02-15_11-55-27'),
-        ('Normalized Manhattan (R38)', 'lablink-hb-norm-manh.csv_best_model_2024-02-23_00-52-51_r38'),
-        ('Normalized Manhattan (R40)', 'lablink-hb-norm-manh.csv_best_model_2024-02-22_02-09-42_r40'),
-        ('SNV (R49)', 'snv_transformed-1.csv_best_model_2024-02-29_22-15-55')
+        ('SNV + br (R49)', 'snv_baseline_removed_pls_top_10_float32.parquet_best_model_2024-03-31_13-29-57'),
+        ('TFLite', 'tflite_model_snv_br_10.tflite'),
+        ('TFLite Q', 'tflite_model_snv_br_10_quant.tflite')
     ]
 
     for label, model_path in model_paths_with_labels:
+
         # Load the model
         model = load_model(model_path)
         # st.write(model)
-
-                # Example conditional block for selecting the correct dataset based on the model's intended use:
-        if label == 'R39':
-            input_data = absorbance_data
-        elif label == 'Normalized Manhattan (R38)':
-            input_data = absorbance_normalized_manh_data
-        elif label == 'Normalized Manhattan (R40)':
-            input_data = absorbance_normalized_manh_data
-        elif label == 'SNV (R49)':
-            input_data = absorbance_snv_data
-        else:
-            continue  # Skip if label does not match expected values
-
-        predictions = predict_with_model(model, input_data)
-        predictions_value = predictions[0][0] 
         
+        # Predict
+        predictions = predict_with_model(model, absorbance_data)
+        predictions_value = predictions[0][0]
     
         st.markdown("""
         <style>
@@ -88,8 +94,8 @@ def main():
         .value {font-size: 60px; font-weight: bold; color: blue;}
         .high-value {font-size: 60px; font-weight: bold; color: red;}
         </style> """, unsafe_allow_html=True)
-
-                # Add condition for prediction value
+    
+        # Add condition for prediction value
         if predictions_value > 25:
             display_value = f'<span class="high-value">High value : ({predictions_value:.1f} g/dL)</span>'
         else:
