4e9fced90c9312b710e23e9026bed5d8276a3da0
hanyswana <146700525+hanyswana@users.noreply.github.com>
2024-05-15 14:19:28 +0800

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -248,7 +248,8 @@ def main():
 
     # Plotting
     plt.figure(figsize=(10, 4))
-    plt.plot(wavelengths, absorbance_snv_baseline_removed_df.iloc[0], marker='o', linestyle='-', color='g', label='Sample')
+    for label, absorbance_snv_baseline_removed in model_results:
+        plt.plot(wavelengths, absorbance_snv_baseline_removed, marker='o', linestyle='-', label=f'Sample {label}')
     plt.plot(wavelengths, Min, linestyle='--', color='r', label='Min')
     plt.plot(wavelengths, Max, linestyle='--', color='y', label='Max')
     plt.title('Preprocessed absorbance', fontweight='bold', fontsize=20)
