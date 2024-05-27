74a54c00a92ee828c5a0a674af6c3d0d35d0b971
hanyswana <146700525+hanyswana@users.noreply.github.com>
2024-05-27 16:02:07 +0800

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -261,7 +261,7 @@ def main():
 
     # Plotting
     plt.figure(figsize=(10, 4))
-    plt.plot(wavelengths, absorbance_snv_normalized_euc_baseline_removed_df.iloc[0], marker='o', linestyle='-', color='g', label='Preprocessed sample')
+    plt.plot(wavelengths, absorbance_values.iloc[0], marker='o', linestyle='-', color='g', label='Preprocessed sample')
     plt.plot(wavelengths, absorbance_df.iloc[0], marker='o', linestyle='-', color='b', label='Raw sample')
     plt.plot(wavelengths, Min, linestyle='--', color='r', label='Min')
     plt.plot(wavelengths, Max, linestyle='--', color='y', label='Max')
