3c9a2daae1afe8935ab40980297637eadbd52eb6
hanyswana <146700525+hanyswana@users.noreply.github.com>
2024-05-29 09:55:38 +0800

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -76,7 +76,7 @@ def json_data():
 
     # CSV ------------------------------------------------------------------------------------------------------------------
     file_path = 'Lablink_134_SNV_norm_eucl_Baseline_sample1.csv'  # Adjust the path if the file is in a specific folder
-    df = pd.read_csv(file_path, usecols=range(3, 19))
+    df = pd.read_csv(file_path, usecols=range(3, 22))
     absorbance_df = df.apply(pd.to_numeric, errors='coerce')
     # absorbance_data = df.iloc[13]
     st.write(absorbance_df)
