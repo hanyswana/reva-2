2cf4189b59704b25208b67a164c76be525819b01
hanyswana <146700525+hanyswana@users.noreply.github.com>
2024-06-18 17:17:45 +0800

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -126,15 +126,15 @@ def json_data():
     absorbance_baseline_removed = baseline_remover.transform(absorbance_snv_df)
     absorbance_baseline_removed_df = pd.DataFrame(absorbance_baseline_removed, columns=absorbance_df.columns)
 
-    # pds_model = joblib.load('pds_model_U6_snv_baseline.joblib')
-    # with open('pds_model_U6_snv_baseline.pkl', 'rb') as f:
-    #     pds_model = pickle.load(f)
+    pds_model = joblib.load('pds_model_U6_snv_baseline.joblib')
+    with open('pds_model_U6_snv_baseline.pkl', 'rb') as f:
+        pds_model = pickle.load(f)
 
-    # absorbance_transformed = pds_transform(absorbance_baseline_removed_df.values, pds_model)
-    # absorbance_transformed_df = pd.DataFrame(absorbance_transformed, columns=absorbance_df.columns)
-    # absorbance_all_pp_df = absorbance_transformed_df
+    absorbance_transformed = pds_transform(absorbance_baseline_removed_df.values, pds_model)
+    absorbance_transformed_df = pd.DataFrame(absorbance_transformed, columns=absorbance_df.columns)
+    absorbance_all_pp_df = absorbance_transformed_df
 
-    absorbance_all_pp_df = absorbance_baseline_removed_df
+    # absorbance_all_pp_df = absorbance_baseline_removed_df
 
     reference_file_path = 'Lablink_134_SNV_Baseline.csv'
     # reference_file_path = 'Lablink_134_SNV_norm_manh_Baseline.csv'
