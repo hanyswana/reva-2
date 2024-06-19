8061af6b030f6ed69db6e37704068d660abcd873
hanyswana <146700525+hanyswana@users.noreply.github.com>
2024-06-19 09:49:39 +0800

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -147,7 +147,7 @@ def json_data(pds_model):
     Min = reference_df.min().values
     Max = reference_df.max().values
  
-    return absorbance_df, absorbance_all_pp_df, wavelengths, golden_values, Min, Max
+    return absorbance_df, absorbance_all_pp_df, wavelengths, golden_values, Min, Max, pds_model
     
 
 def create_csv(golden_values, Min, Max, wavelengths):
