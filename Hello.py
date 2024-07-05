234eca4f161a1e2d954ff40283feb6eb270f5d4a
hanyswana <146700525+hanyswana@users.noreply.github.com>
2024-07-05 11:01:51 +0800

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -262,8 +262,8 @@ def main():
         # st.write(prediction_data)
 
         # TF/TFLITE MODEL ---------------------------------------------------------------------------------
-        model = load_model(model_path)
-        predictions = predict_with_model(model, prediction_data)
+        # model = load_model(model_path)
+        # predictions = predict_with_model(model, prediction_data)
 
         # TABNET MODEL ---------------------------------------------------------------------------------
         model = load_tabnet_model(model_path)
