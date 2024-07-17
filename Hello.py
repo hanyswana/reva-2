10fe469220dfe1875c1ef415128644fa1150dddb
hanyswana <146700525+hanyswana@users.noreply.github.com>
2024-07-17 16:30:53 +0800

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -288,9 +288,9 @@ def main():
         # ('SNV + BR (tf-R50)', 'Lablink_134_SNV_Baseline_pls_top_10.parquet_best_model_2024-05-18_04-08-04_R50_78%'),
         # ('SNV +  + norm euc + BR (tf-R52)', 'Lablink_134_SNV_norm_eucl_Baseline_pls_top_10.parquet_best_model_2024-05-24_05-21-44_R52_78%')
         # ('SNV + norm manh + BR (tf-R52)', 'Lablink_134_SNV_norm_manh_Baseline_pls_top_10.parquet_best_model_2024-05-27_19-43-51_R52_85%')
-        ('SNV + BR (pt)', 'Lablink_134_SNV_Baseline_pls_top_10_2024-06-06_14-42-37.pt.zip'),
-        ('SNV + BR (onnx)', 'model_snv_br_2024-06-06_14-42-37.onnx'),
-        ('SNV + BR (tflite)', 'model_snv_br_2024-06-06_14-42-37.tflite'),
+        ('SNV + BR (pt)', 'tabnet-model/Lablink_134_SNV_Baseline_pls_top_10_2024-06-06_14-42-37.pt.zip'),
+        ('SNV + BR (onnx)', 'tabnet-model/model_snv_br_2024-06-06_14-42-37.onnx'),
+        ('SNV + BR (tflite)', 'tabnet-model/model_snv_br_2024-06-06_14-42-37.tflite'),
         ('SNV + BR (tflite-quant)', 'tabnet-model/model_snv_br_2024-06-06_14-42-37_quant.tflite')
         # ('SNV + BR (tf)', 'model_snv_br_2024-06-06_14-42-37')
         # ('SNV +  + norm euc + BR (tf-R53)', 'corrected-lablink-128-hb_SNV_norm_eucl_Baseline_top_10.parquet_best_model_2024-07-09_22-18-50_R53_88%') # correct dataset
