43a7778917dbb14404cba54ff2e5b0c71a89e301
hanyswana <146700525+hanyswana@users.noreply.github.com>
2024-07-18 10:39:28 +0800

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -149,9 +149,8 @@ def json_data():
     # st.write(absorbance_all_pp_df)
 
     reference_file_path = 'correct-data/corrected-lablink-128-hb_SNV_Baseline.csv'
-    # reference_file_path = 'incorrect-data/Lablink_134_SNV_Baseline.csv'
-    # reference_file_path = 'Lablink_134_SNV_norm_manh_Baseline.csv'
-    # reference_file_path = 'Lablink_134_SNV_norm_eucl_Baseline.csv'
+    # reference_file_path = 'correct-data/corrected-lablink-128-hb_SNV_norm_manh_Baseline.csv'
+    # reference_file_path = 'correct-data/corrected-lablink-128-hb_SNV_norm_eucl_Baseline.csv'
     reference_df = pd.read_csv(reference_file_path, usecols=range(3, 22))
     reference_df = reference_df.apply(pd.to_numeric, errors='coerce')
     
